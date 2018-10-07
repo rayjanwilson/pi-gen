776eba6f5387d129773f873d512da5537a083593
Rayjan Wilson <rayjan.wilson@gmail.com>
2018-10-07 13:06:29 -0400

    adds script for installing build system deps

---
+++ install-deps.sh
@@ -0,0 +1,19 @@
+sudo apt-get install \
+build-essential \
+quilt \
+parted \
+realpath \
+qemu-user-static \
+debootstrap \
+zerofree \
+pxz \
+zip \
+dosfstools \
+libcap2-bin \
+bsdtar \
+grep \
+rsync \
+xz-utils
+curl \
+file \
+git
