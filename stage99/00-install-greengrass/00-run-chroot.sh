33c2f3888ee2d4363d63665860fe7b92259b5e1e
Rayjan Wilson <rayjan.wilson@gmail.com>
2018-10-07 13:44:02 -0400

    changes the install file command

--- stage99/00-install-greengrass/00-run-chroot.sh
+++ stage99/00-install-greengrass/00-run-chroot.sh
@@ -7,7 +7,7 @@ adduser ggc_user ggc_group
 #give greengrass user access to the gpu and video system
 #usermod -a -G video,spi,i2c,gpio ggc_user
 
-install -m 755 files/greengrass "${ROOTFS_DIR}/etc/init.d/greengrass"
+install -m 755 files/greengrass "${ROOTFS_DIR}/etc/init.d/"
 
-install files/greengrass.service "${ROOTFS_DIR}/etc/systemd/system/greengrass.service"
+install files/greengrass.service "${ROOTFS_DIR}/etc/systemd/system/"
 systemctl enable greengrass.service
