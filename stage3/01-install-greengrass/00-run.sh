37e4397e1898e6481b8b27f924517370b7036f62
Rayjan Wilson <rayjan.wilson@gmail.com>
2018-10-07 22:50:54 -0400

    printing out groups

--- stage3/01-install-greengrass/00-run.sh
+++ stage3/01-install-greengrass/00-run.sh
@@ -2,26 +2,25 @@
 
 #give greengrass user access to the gpu and video system
 #usermod -a -G video,spi,i2c,gpio ggc_user
+groups
 
 install -m 755 files/greengrass "${ROOTFS_DIR}/etc/init.d/"
-install files/greengrass.service "${ROOTFS_DIR}/etc/systemd/system/"
+install -m 555 files/greengrass.service "${ROOTFS_DIR}/etc/systemd/system/"
 install files/98-rpi.conf "${ROOTFS_DIR}/etc/sysctl.d/"
 
-#
-# tar Czxf ${ROOTFS_DIR} files/greengrass*.tar.gz
-#
-# # Users should place the extracted keys/config into the fat32 partition.
-# rm -rf ${ROOTFS_DIR}/greengrass/certs \
-#        ${ROOTFS_DIR}/greengrass/config
-# mkdir -p ${ROOTFS_DIR}/boot/greengrass/certs
-# mkdir -p ${ROOTFS_DIR}/boot/greengrass/config
-#
-# # copy root cert into /greengrass/certs
-# cp files/root.ca.pem ${ROOTFS_DIR}/greengrass/certs
-#
-# # turn on i2c, spi, and bump gpu memory
-install -m 755 files/config.txt "${ROOTFS_DIR}/boot/"
+tar Czxf ${ROOTFS_DIR} files/greengrass*.tar.gz
 
+# Users should place the extracted keys/config into the fat32 partition.
+rm -rf ${ROOTFS_DIR}/greengrass/certs \
+       ${ROOTFS_DIR}/greengrass/config
+mkdir -p ${ROOTFS_DIR}/boot/greengrass/certs
+mkdir -p ${ROOTFS_DIR}/boot/greengrass/config
+
+# copy root cert into /greengrass/certs
+install files/root.ca.pem "${ROOTFS_DIR}/greengrass/certs/"
+
+# turn on i2c, spi, and bump gpu memory
+install -m 755 files/config.txt "${ROOTFS_DIR}/boot/"
 
 # enable ssh
 touch ${ROOTFS_DIR}/boot/ssh
