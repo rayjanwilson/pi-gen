7f231ab7c8af81979df2a065a96f86c334ce22db
Rayjan Wilson <rayjan.wilson@gmail.com>
2018-10-13 10:10:33 -0400

    makes executable

--- stage4/02-install-node/00-run.sh
+++ stage4/02-install-node/00-run.sh
@@ -1,6 +1,7 @@
 
 echo "Unpacking node.js for arm7l"
 curl -sL https://deb.nodesource.com/setup_10.x | bash -
+apt-get update
 #apt-get install -y nodejs
 #tar CJxf ${ROOTFS_DIR} files/node*.tar.xz --strip-components=1
 echo "done"
