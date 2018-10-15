6f0caf210d04f600458a6b2da46cc16be390e0b2
Rayjan Wilson <rayjan.wilson@gmail.com>
2018-10-15 00:40:19 -0400

    dops comments

--- stage4/02-install-node/00-run-chroot.sh
+++ stage4/02-install-node/00-run-chroot.sh
@@ -1,8 +1,7 @@
 
 echo "Unpacking node.js for arm7l"
-# on_chroot << EOF
+
 curl -sL https://deb.nodesource.com/setup_10.x | bash -
-# EOF
-#apt-get install -y nodejs
+
 #tar CJxf ${ROOTFS_DIR} files/node*.tar.xz --strip-components=1
 echo "done"
