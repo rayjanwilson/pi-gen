3057b76681d742a45920eebd3fdbf0fb3056ff46
Rayjan Wilson <rayjan.wilson@gmail.com>
2018-10-14 23:41:30 -0400

    moving directly into run-chroot

--- stage4/02-install-node/00-run-chroot.sh
+++ stage4/02-install-node/00-run-chroot.sh
@@ -1,8 +1,8 @@
 
 echo "Unpacking node.js for arm7l"
-on_chroot << EOF
+# on_chroot << EOF
 curl -sL https://deb.nodesource.com/setup_10.x | bash -
-EOF
+# EOF
 #apt-get install -y nodejs
 #tar CJxf ${ROOTFS_DIR} files/node*.tar.xz --strip-components=1
 echo "done"
