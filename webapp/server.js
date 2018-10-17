0e40183b49ebc6d239afef28d724f52a56da99df
Rayjan Wilson <rayjan.wilson@gmail.com>
2018-10-16 21:37:46 -0400

    adding form and iwlist

--- webapp/server.js
+++ webapp/server.js
@@ -1,13 +1,27 @@
 #!/usr/bin/node
+
 const express = require('express');
 const os = require('os');
 const path = require('path');
 const fs = require('fs');
 const bodyParser = require('body-parser')
+const iwlist = require("./iwlist")
 const app = express();
 
+function log_error_send_success_with(success_obj, error, response) {
+  if (error) {
+    console.log("ERROR: " + error);
+    response.send({status: "ERROR", error: error});
+  } else {
+    success_obj = success_obj || {};
+    success_obj["status"] = "SUCCESS";
+    response.send(success_obj);
+  }
+  response.end();
+}
+
 app.use(bodyParser.json());
-app.use('/static',express.static(path.join(__dirname,'/build/static')))
+app.use('/static', express.static(path.join(__dirname, '/build/static')))
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/build/index.html'));
@@ -15,13 +29,18 @@ app.get('/', (req, res) => {
 
 app.post('/saveWifi', (req, res) => {
   let configs = JSON.stringify(req.params);
-  fs.writeFile('/boot/webConfigs.txt', configs, (err) => {
-    if (err) throw err;
+  fs.writeFile('./webConfigs.txt', configs, (err) => {
+    if (err)
+      throw err;
     console.log('config saved')
   });
   res.send(req.params);
 });
-app.get('/api/getUsername', (req, res) => {
-  res.send({ username: os.userInfo().username })
+app.get('/api/listWifi', (req, res) => {
+  iwlist((error, result) => {
+    console.log(result)
+    // log_error_send_success_with(result[0], error, response);
+  });
+  // res.send({username: os.userInfo().username})
 });
-app.listen(80, () => console.log('Listening on port 80!'));
+app.listen(3000, () => console.log('Listening on port 3000!'));
--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,24 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      ssid: '',
+      pw: ''
+    }
+  }
+  handleChange = (e) => {
+    const target = e.target;
+    const name = target.name
+    const value = target.type === 'checkbox'
+        ? target.checked
+        : target.value
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit = (e) => {
+    console.log('SSID: ' + this.state.ssid);
+    console.log('PW: ' + this.state.pw);
+    e.preventDefault();
+    // fetch(url, {
+    //   method: "POST",
+    //   body: JSON.stringify(data),
+    //   headers: {
+    //     "Content-Type": "application/json"
+    //   },
+    //   credentials: "same-origin"
+    // }).then((response) => {
+    //   response.status     //=> number 100–599
+    //   response.statusText //=> String
+    //   response.headers    //=> Headers
+    //   response.url        //=> String
+    //
+    //   return response.text()
+    // }, (error) => {
+    //   error.message //=> String
+    // })
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <form onSubmit={this.handleSubmit}>
+            <label>SSID:
+              <input type="text" name="ssid" value={this.state.ssid} onChange={this.handleChange}/>
+            </label>
+            <br />
+            <label>PW:
+              <input type="text" name="pw" value={this.state.pw} onChange={this.handleChange}/>
+            </label>
+            <br />
+            <input type="submit" value="Submit" />
+          </form>
         </header>
       </div>
     );
