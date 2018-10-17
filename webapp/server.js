f90c390d0f6ff255eef665d61ab5c4c8adf428e0
Rayjan Wilson <rayjan.wilson@gmail.com>
2018-10-17 12:10:11 -0400

    got async await working

--- webapp/server.js
+++ webapp/server.js
@@ -4,42 +4,37 @@ const express = require('express');
 const os = require('os');
 const path = require('path');
 const fs = require('fs');
-const bodyParser = require('body-parser')
-const iwlist = require("./iwlist")
+const bodyParser = require('body-parser');
 const app = express();
+const iwlistAsync = require('./iwlistAsync');
 
-function log_error_send_success_with(success_obj, error, response) {
-  if (error) {
-    console.log("ERROR: " + error);
-    response.send({status: "ERROR", error: error});
-  } else {
-    success_obj = success_obj || {};
-    success_obj["status"] = "SUCCESS";
-    response.send(success_obj);
-  }
-  response.end();
-}
+const asyncMiddleware = fn =>
+  (req, res, next) => {
+    Promise.resolve(fn(req, res, next))
+      .catch(next);
+  };
 
 app.use(bodyParser.json());
 app.use('/static', express.static(path.join(__dirname, '/build/static')))
 
-app.get('/', (req, res) => {
+app.get('/', asyncMiddleware( async (req, res, next) => {
   res.sendFile(path.join(__dirname + '/build/index.html'));
-});
+}));
 
-app.post('/saveWifi', (req, res) => {
-  let configs = JSON.stringify(req.params);
-  fs.writeFile('./webConfigs.txt', configs, (err) => {
-    if (err)
-      throw err;
-    console.log('config saved')
-  });
-  res.send(req.params);
-});
-app.get('/api/listWifi', (req, res) => {
-  iwlist((error, result) => {
-    console.log(result)
-    log_error_send_success_with(result[0], error, res);
-  });
-});
+app.get('/listWifi', asyncMiddleware( async (req, res, next) => {
+  const wifis = await iwlistAsync();
+  console.log(wifis);
+  res.json(wifis);
+}));
+
+// app.post('/saveWifi', (req, res) => {
+//   let configs = JSON.stringify(req.params);
+//   fs.writeFile('./webConfigs.txt', configs, (err) => {
+//     if (err)
+//       throw err;
+//     console.log('config saved')
+//   });
+//   res.send(req.params);
+// });
+
 app.listen(3000, () => console.log('Listening on port 3000!'));
